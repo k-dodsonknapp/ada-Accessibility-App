5265ac1dae46eed5aeda0f29463695a8ae530465
Tedi Lowney <tmlowney1@gmail.com>
2022-10-15 16:08:29 -0400

    front end routing fixed

--- ada-accessability-app/src/components/MainContent.js
+++ ada-accessability-app/src/components/MainContent.js
@@ -1,11 +1,15 @@
 import "../App.css";
-import { Home } from "./views";
-import { Routes, Route, Link } from "react-router-dom";
+import { Home, Contact, Tracker, Resources, Community } from "./views";
+import { Routes, Route } from "react-router-dom";
 
 const MainContent = () => {
   return (
     <div className="routes">
       <Routes>
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/tracker" element={<Tracker />} />
+        <Route exact path="/resources" element={<Resources />} />
+        <Route exact path="/community" element={<Community />} />
         <Route exact path="/" element={<Home />} />
       </Routes>
     </div>
--- ada-accessability-app/src/components/views/Tracker.js
+++ ada-accessability-app/src/components/views/Tracker.js
@@ -13,3 +13,5 @@ const Tracker = () => {
     </div>
   );
 };
+
+export default Tracker;
