14f7509ec1e4b42df564b116f091ef2acf5c3a5f
Tedi Lowney <tmlowney1@gmail.com>
2022-10-15 12:42:45 -0400

    put React logo back

--- ada-accessability-app/src/App.js
+++ ada-accessability-app/src/App.js
@@ -1,5 +1,5 @@
-// import logo from "./logo.svg";
-import logo from "./TeamShotADA.png";
+import logo from "./logo.svg";
+// import logo from "./TeamShotADA.png";
 import "./App.css";
 
 function App() {
