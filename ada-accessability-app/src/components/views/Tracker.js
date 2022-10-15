3025feda46408d60a5e84b2fab954c95c682835f
Tedi Lowney <tmlowney1@gmail.com>
2022-10-15 16:36:20 -0400

    styles for tracker component

--- ada-accessability-app/src/components/views/Tracker.js
+++ ada-accessability-app/src/components/views/Tracker.js
@@ -1,15 +1,19 @@
+import "../styles/tracker.css";
+
 const Tracker = () => {
   return (
-    <div className="week">
-      <div className="Monday">
-        <div className="entry"></div>
-        <div className="entry"></div>
-        <div className="entry"></div>
+    <div className="tracker-page-wrapper">
+      <div className="week">
+        <div className="Monday">
+          <div className="entry">dfdfsd</div>
+          <div className="entry"></div>
+          <div className="entry"></div>
+        </div>
+        <div className="Tuesday"></div>
+        <div className="Wednesday"></div>
+        <div></div>
+        <div></div>
       </div>
-      <div className="Tuesday"></div>
-      <div className="Wednesday"></div>
-      <div></div>
-      <div></div>
     </div>
   );
 };
