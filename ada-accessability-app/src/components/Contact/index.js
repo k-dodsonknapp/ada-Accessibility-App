c1298e7c914f7fb4b4cde09be0a138e36179b269
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2022-10-15 20:47:53 -0700

    styled the contact page

--- ada-accessability-app/src/components/Contact/index.js
+++ ada-accessability-app/src/components/Contact/index.js
@@ -1,7 +1,49 @@
 import "../../App.css";
+import { NavLink } from 'react-router-dom'
+import "./contact.css"
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className="contact-wrapper">
+      <h2>Got questions? We're here to help.</h2>
+      <p>Checkout out our <NavLink to='/resources'>Resources</NavLink> for some quick answers. If you need additional help please fill out the form below or give us a call at <span>800-123-4567</span></p>
+      <div className="form-wrapper">
+
+        <form className="contact-form">
+          <label>Name</label>
+          <input></input>
+          <label>Email</label>
+          <input></input>
+          <label>Type of contact</label>
+          <input></input>
+          <label>Message</label>
+          <textarea></textarea>
+
+          <div className="btn-div">
+            <button typeof="submit">Submit</button>
+          </div>
+        </form>
+      </div>
+      <div className="crisis-contact">
+        <h3>If you are having a medical emergency, please contact:</h3>
+        <div className="contact=address">
+          <div className="contact">
+            <h4>Suicide Hotline:</h4>
+            <a href="https://988lifeline.org/">https://988lifeline.org/</a>
+          </div>
+          <div className="contact">
+            <h4>Crisis Text-Line:</h4>
+            <a href='https://www.crisistextline.org/'>https://www.crisistextline.org/</a>
+          </div>
+          <div className="contact">
+            <h4>Crisis Hotline:</h4>
+            <a href='https://www.apa.org/topics/crisis-hotlines'>https://www.apa.org/topics/crisis-hotlines</a>
+          </div>
+        </div>
+        {/* <h4>Suicide Hotline: <Link></Link></h4> */}
+      </div>
+    </div>
+  );
 };
 
 export default Contact;
--- ada-accessability-app/src/components/Footer/index.js
+++ ada-accessability-app/src/components/Footer/index.js
@@ -1,8 +1,35 @@
 import React from "react";
-// import "./footer.css";
+import "./footer.css";
+import logo from "./logoRppl.png";
+import { NavLink } from 'react-router-dom'
 
 function Footer() {
-  return <div className="footer"></div>;
+  return (
+    <div className="footer">
+        <img className="foot-logo-img" src={logo}></img>
+        <ul className="site-map-UL">
+          <title>Site Map</title>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact">Contact</NavLink>
+          </li>
+          <li>
+            <NavLink to='/tracker'>Tracker</NavLink>
+          </li>
+          <li>
+            <NavLink to="/resources">Resources</NavLink>
+          </li>
+          <li>
+            <NavLink to="community">Community</NavLink>
+          </li>
+        </ul>
+    </div>
+  )
 }
 
 export default Footer;
