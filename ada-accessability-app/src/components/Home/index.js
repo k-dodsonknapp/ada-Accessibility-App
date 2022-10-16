8c66c812da4b9251c1b7dbfba10aab7400b96301
Kenneth Dodson-Knapp <kdk52391@gmail.com>
2022-10-15 19:10:39 -0700

    styled home page

--- ada-accessability-app/src/components/Home/index.js
+++ ada-accessability-app/src/components/Home/index.js
@@ -1,8 +1,10 @@
 import "../../App.css";
 import './home.css'
+import { NavLink } from 'react-router-dom';
 import { CiViewList } from "react-icons/ci";
 import { GiWhiteBook } from "react-icons/gi";
-import { IoIosPeople } from "react-icons/io";
+import { IoIosPeople, IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
+import { FaRegUserCircle } from "react-icons/fa";
 
 
 
@@ -17,28 +19,50 @@ const Home = () => {
           <button className="CTA-btn">Call to action</button>
         </div>
         <div className="img-wrapper">
-          <img className='hero-image' src="https://thedoahnewspaper.files.wordpress.com/2022/05/download-3.jpeg"></img>
+          <img className='hero-image' src="https://thedoahnewspaper.files.wordpress.com/2022/05/download-3.jpeg" alt=""></img>
         </div>
       </div>
       <div className="features-wrapper">
-        <h2>Our Features</h2>
+        <h2>OUR FEATURES</h2>
         <div className="features-type">
           <div className='feature-content'>
-            <CiViewList />
+            <CiViewList className="feature-icon" />
             <h3>Track</h3>
+            <p>Personalized journal, record, mood, track symptoms, spot patterns, gain insights</p>
+            <NavLink to=''>Learn More</NavLink>
           </div>
           <div className='feature-content'>
+            <GiWhiteBook className="learn-icon" />
             <h3>Learn</h3>
-            <GiWhiteBook />
+            <p>Personalized resources, mental health education, support for marginalized groups</p>
+            <NavLink id='learn-link' to=''>Learn More</NavLink>
           </div>
           <div className='feature-content'>
+            <IoIosPeople className="feature-icon" />
             <h3>Connect</h3>
-            <IoIosPeople />
+            <p>Build community, share progress, lead discussions, better together</p>
+            <NavLink to=''>Learn More</NavLink>
           </div>
         </div>
       </div>
       <div className="testimonials-wrapper">
-
+        <h2>WHAT PEOPLE ARE SAYING</h2>
+        <div className="testimonials-content">
+        <IoIosArrowBack className="arrow-back"/>
+          <div className="user-story">
+            <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. In id diam sit amet nisl tincidunt blandit.” </p>
+            <h4 className="name"><span className="name-span"><FaRegUserCircle/></span> Emily</h4>
+          </div>
+          <div className="user-story">
+            <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. In id diam sit amet nisl tincidunt blandit.” </p>
+            <h4 className="name"><span className="name-span"><FaRegUserCircle/></span> George</h4>
+          </div>
+          <div className="user-story">
+            <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. In id diam sit amet nisl tincidunt blandit.” </p>
+            <h4 className="name"><span className="name-span"><FaRegUserCircle/></span> Ana</h4>
+          </div>
+        <IoIosArrowForward style={{color: "#002627"}} className="arrow-forward"/>
+        </div>
       </div>
     </div>
 
